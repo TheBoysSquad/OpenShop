ac62ccccbbcfe4e050a5d9dd5d83087de51871c9
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2023-05-05 15:28:07 -0500

    Backend Update
    2.0

---
+++ Backend---04052023-master/src/index.js
@@ -0,0 +1,9 @@
+// Aplicacion de servidor backend 
+
+//// Aplicacion backend creada por {Andres Camacho}
+const server = require('./server/server');
+const servidor = server();
+const database = require('./database/database');
+
+
+
